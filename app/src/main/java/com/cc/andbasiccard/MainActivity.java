9d42afe122fea78b04c36a7e0ee4505df7af4b42
kartikohri1712 <kartikohri13@gmail.com>
2018-02-27 20:30:34 +0530

    Added comments to explain functioning

--- app/src/main/java/com/cc/andbasiccard/MainActivity.java
+++ app/src/main/java/com/cc/andbasiccard/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         setContentView(R.layout.activity_main);
 
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        //Populate the spinner with items
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.fonts,
                 android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -28,9 +29,12 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onItemSelected(AdapterView<?> parent, View view,
                                int pos, long id) {
+        // Get the item selected by user using parent.getItemAtPosition(pos)
+        //Perform relevant operations to obtain font name
         String mg = ((String) parent.getItemAtPosition(pos)).trim();
         mg = (mg.replace(' ', '_')).toLowerCase();
         int m = getResources().getIdentifier(mg, "font", getPackageName());
+        // set the selected fontfamily
         Typeface typeface = ResourcesCompat.getFont(this, m);
         TextView text = findViewById(R.id.textView);
         text.setTypeface(typeface);
